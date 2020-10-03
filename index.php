932dbf5bfc0fda1f6a4773119e5150cbdffef042
Simon Dubois <simon@dubandubois.com>
2020-10-03 20:16:13 +0200

    Display hand success as table

--- index.php
+++ index.php
@@ -2,12 +2,27 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
+use App\Hand;
+use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\SingleCommandApplication;
 
 (new SingleCommandApplication())
     ->setCode(function (InputInterface $input, OutputInterface $output) {
-        //
+        $rows = [];
+
+        foreach (range(0, 6) as $successDices) {
+            $hand = new Hand($successDices);
+            $rows[$successDices][] = str_repeat('⧫', $successDices) . str_repeat('◊', 6 - $successDices);
+            foreach (range(10, 20, 2) as $col => $target) {
+                $rows[$successDices][] = str_pad(round($hand->success($target) * 100) . ' %', 5, ' ', STR_PAD_LEFT);
+            }
+        }
+
+        (new Table($output))
+            ->setHeaders(array_merge([''], range(10, 20, 2)))
+            ->setRows($rows)
+            ->render();
     })
     ->run();
